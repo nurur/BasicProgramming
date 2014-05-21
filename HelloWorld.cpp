ea9e9ad7e991f3e84fb89179a9ba0fbf1acebf77
M Nurur Rahman <nurur@yahoo.com>
2014-05-21 16:01:49 -0400

    basic programming file01

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+// Hello World Program in C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+// Main function 
+int main(int argc, char* argv[])
+{
+
+  string s = "Hello World!";
+  cout << s << endl;
+
+  return 0;
+}
---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# Hello World Program in Python
+
+#Method 1
+#Use print command 
+print 'Hello World!'
+print '            '
+
+
+#Method 2 
+#First define a function 
+def fun():
+    print 'Hello World!'
+
+#Next call the function 
+fun()
